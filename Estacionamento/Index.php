045af7c9e00439ff22099345a618de16627fe0c1
daniel <raizsuporte_daniel@hotmail.com>
2016-03-07 21:54:17 -0300

    Alteração na página Index.php do projeto.

--- Estacionamento/Index.php
+++ Estacionamento/Index.php
@@ -1,3 +1,10 @@
-<?php
-echo "Página inicial do projeto.";
-?>
+<!DOCTYPE html>
+<html lang='pt-BR'>
+<head>
+<meta charset='UTF-8'>
+<title>Teste</title>
+</head>
+<body>
+<h1>Palmeiras não tem mundial.</h1>
+</body>
+</html>
